9000c7f893e6c6eda2ae77d14eab827fbbb88b53
นาย ธันวา ชัยรัตนานนท์ (Tanwa Chairattananon) <141398800+Owenwa348@users.noreply.github.com>
2025-08-03 02:53:24 +0700

    feat: Enhance Assessment view with API integration for submission checks and data handling

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import axios from 'axios'
 
 import './style.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -16,5 +17,12 @@ const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 app.mount('#app')
 
